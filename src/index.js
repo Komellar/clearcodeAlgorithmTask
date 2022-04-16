04e9316b07eb0d09b1678d92cb58ceaa4c650f6d
Komellar <61510575+Komellar@users.noreply.github.com>
2022-04-16 19:25:47 +0200

    Implement nested checking

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ export const compare = (prev, next) => {
       if (typeof value !== "object") {
         result[key] = value !== next[key];
       } else if (Array.isArray(value)) {
-        arrayLoop(value, key, nextObj[key]);
+        arrayLoop(value, key, nextObj[key], result);
       } else {
-        objectLoop(value, key, nextObj[key]);
+        objectLoop(value, key, nextObj[key], result);
       }
     }
   }
 
   function arrayLoop(value, key, nextObj, tempResultObj) {
     console.log("-------ARRAY LOOP-------");
-    // console.log("value", value);
-    // console.log("nextObj", nextObj);
     console.log("key", key);
     console.log("tempResult", tempResultObj);
 
@@ -26,45 +24,44 @@ export const compare = (prev, next) => {
       if (value.length !== nextObj.length) {
         console.log("here1");
         tempResultObj[key] = true;
-        // result[key] = true;
       } else {
         console.log("here2");
         const tempResults = value.map((item, index) => item !== nextObj[index]);
-        // result[key] = tempResults.includes(true) ? true : false;
         tempResultObj[key] = tempResults.includes(true) ? true : false;
       }
     } else {
       console.log("IT HAS OBJECT");
-      objectLoop(value, key, nextObj);
+      objectLoop(value, key, nextObj, tempResultObj);
     }
   }
 
-  function objectLoop(value, key, nextObj) {
-    const tempResultObj = {};
-    result[key] = {};
+  function objectLoop(value, key, nextObj, tempResultObj = {}) {
+    tempResultObj[key] = {};
     console.log("------OBJECT LOOP-------");
-    // console.log("value: ", value);
-    // console.log("nextObj", nextObj);
+    console.log("tempResultObj", tempResultObj);
 
     for (const [nestedKey, nestedValue] of Object.entries(value)) {
       if (typeof nestedValue !== "object") {
-        // console.log("nestedValue:", nestedValue);
         console.log("nestedKey: ", nestedKey);
         console.log("key:", key);
 
-        tempResultObj[nestedKey] = nestedValue !== nextObj[nestedKey];
-        // console.log(tempResultObj);
-
-        // result[key][nestedKey] = nestedValue !== nextObj[nestedKey];
+        tempResultObj[key][nestedKey] = nestedValue !== nextObj[nestedKey];
       } else if (Array.isArray(nestedValue)) {
-        arrayLoop(nestedValue, nestedKey, nextObj[nestedKey], tempResultObj);
+        arrayLoop(
+          nestedValue,
+          nestedKey,
+          nextObj[nestedKey],
+          tempResultObj[key]
+        );
+      } else {
+        objectLoop(
+          nestedValue,
+          nestedKey,
+          nextObj[nestedKey],
+          tempResultObj[key]
+        );
       }
-      // else {
-      // objectLoop(nestedValue, nestedKey, nextObj[nestedKey]);
-      // }
     }
-    result[key] = tempResultObj;
-    console.log(tempResultObj);
   }
 
   mainLoop(prev, next);
@@ -73,6 +70,26 @@ export const compare = (prev, next) => {
 };
 
 const prev1 = {
+  uuid: "1013251d",
+  agency: {
+    uuid: "b6ca2ee1",
+    name: "Agency 2",
+    tags: ["one", "two"],
+    accounts: 2
+  }
+};
+
+const next1 = {
+  uuid: "1013251d",
+  agency: {
+    uuid: "b6ca2ee1",
+    name: "Agency 1",
+    tags: ["one", "two", "three"],
+    accounts: 2
+  }
+};
+
+const prev = {
   uuid: "1013251d",
   name: "Campaign 1",
   total_budget_amount: 123456700,
@@ -84,10 +101,25 @@ const prev1 = {
     name: "Agency 1",
     tags: ["one", "two"],
     accounts: 2
-  }
+  },
+  line_items: [
+    {
+      uuid: "1ab24a77",
+      name: "Line Item #1",
+      type: "DISPLAY",
+      whitelist: ["https://google.com", "https://yahoo.com"],
+      active: false,
+      budget_amount: 12345600,
+      payment_model: {
+        uuid: "1a5f0a9b-b3a8-43e0-b0d7-419e70ab929c",
+        name: "CPC",
+        code: "CPC"
+      }
+    }
+  ]
 };
 
-const next1 = {
+const next = {
   uuid: "1013251d",
   name: "Campaign 2",
   total_budget_amount: 2423,
@@ -99,8 +131,37 @@ const next1 = {
     name: "Agency 1",
     tags: ["one", "two", "three"],
     accounts: 2
-  }
+  },
+  line_items: [
+    {
+      uuid: "1ab24a77",
+      name: "Line Item #1",
+      type: "VIDEO",
+      whitelist: ["https://yahoo.com"],
+      active: false,
+      budget_amount: 12345600,
+      payment_model: {
+        uuid: "1a5f0a9b",
+        name: "CPC",
+        code: "CPC"
+      }
+    },
+    {
+      uuid: "2bb02s125",
+      name: "Line Item #2",
+      type: "VIDEO",
+      whitelist: ["https://yahoo.com"],
+      active: true,
+      budget_amount: 58890,
+      payment_model: {
+        uuid: "1a5f0a9b",
+        name: "CPV",
+        code: "CPV"
+      }
+    }
+  ]
 };
 
-const comparisonResult = compare(prev1, next1);
+// const comparisonResult = compare(prev1, next1);
+const comparisonResult = compare(prev, next);
 console.log(comparisonResult);
