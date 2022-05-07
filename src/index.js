6300d042937dcb9dd8dc22e69c75ca04fecfc267
Komellar <61510575+Komellar@users.noreply.github.com>
2022-05-07 11:18:00 +0200

    Fix small bug (pass array instead of obj)

--- src/index.js
+++ src/index.js
@@ -14,12 +14,13 @@ export const compare = (prev, next) => {
   }
 
   function arrayLoop(value, key, nextObj, tempResultObj) {
+    tempResultObj[key] = [];
     if (value.every((x) => typeof x !== "object")) {
       if (value.length !== nextObj?.length) {
         tempResultObj[key] = true;
       } else {
         const tempResults = value.map((item, index) => item !== nextObj[index]);
-        tempResultObj[key] = tempResults.includes(true) ? true : false;
+        tempResultObj[key].push(tempResults.includes(true));
       }
     } else {
       objectLoop(value, key, nextObj, tempResultObj);
@@ -55,52 +56,3 @@ export const compare = (prev, next) => {
 
   return result;
 };
-
-//
-// You can test it like below. Just uncomment and pass your objects
-//
-
-// const prev = {
-//   uuid: "1013251d",
-//   name: "Campaign 1",
-//   total_budget_amount: 123456700,
-//   start_date: "2022-03-11T13:37:37Z",
-//   end_date: "2022-03-31T12:37:37Z",
-//   active: true,
-//   agency: {
-//     uuid: "b6ca2ee1",
-//     name: "Agency 1",
-//     tags: ["one", "two"],
-//     accounts: 2
-//   },
-//   line_items: [
-//     {
-//       uuid: "1ab24a77",
-//       name: "Line Item #1",
-//       type: "DISPLAY",
-//       whitelist: ["https://google.com", "https://yahoo.com"],
-//       active: false,
-//       budget_amount: 12345600,
-//       payment_model: {
-//         uuid: "1a5f0a9b",
-//         name: "CPC",
-//         code: "CPC"
-//       }
-//     }
-//   ]
-// };
-
-// const next = {
-//   uuid: "1013251d",
-//   name: "Campaign 2",
-//   total_budget_amount: 2423,
-//   end_date: "2022-03-31T12:37:37Z",
-//   active: false,
-//   agency: {
-//     uuid: "b6ca2ee1",
-//     name: "Agency 1",
-//     accounts: 2
-//   }
-// };
-
-// console.log(compare(prev, next));
